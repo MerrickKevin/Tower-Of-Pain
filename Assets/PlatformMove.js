b87445336cc0b44dcecceda3a4f2650691bdfc2f
Kevin Merrick <nxtkevin.merrick@gmail.com>
2016-10-31 17:03:59 -0600

    Message!

---
+++ Assets/PlatformMove.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+    private var rb: Rigidbody;
+    var speed = 0;
+function Start () {
+    rb = GetComponent(Rigidbody);
+}
+
+function FixedUpdate () {
+    var moveForce = new Vector3(2,0,0);
+    rb.AddForce(moveForce);
+}
---
+++ Assets/Scripts/PlayerControl.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+
+
+    var speed = 0;
+    var jumpForce = 0;
+    private var rb : Rigidbody;
+
+function Start () {
+    rb = GetComponent(Rigidbody);
+    speed = 5;
+    jumpForce = 400;
+}
+function Update(){
+if (Input.GetKeyDown("space")){
+        var jump = new Vector3(0,jumpForce,0);
+        rb.AddForce(jump);
+    }
+
+}
+
+function FixedUpdate(){
+    
+    var moveHorizontal = Input.GetAxis ("Horizontal");
+    var moveVertical = Input.GetAxis ("Vertical");
+    var movement = new Vector3(moveHorizontal,0,moveVertical);
+    rb.AddForce(movement * speed);
+}
