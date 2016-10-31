489e102decb75b23c09800f5b0b40bc4cef22d10
Alex <mrclean.durrant@gmail.com>
2016-10-31 17:13:00 -0600

    Added new script to tell kevin to fuck off.

---
+++ Assets/ThisIsTest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThisIsTest : MonoBehaviour
+{
+
+    public bool KevinFuckOff;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
